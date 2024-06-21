a06d5c57fa01c5ecf48fd9a191ba0ba0b062c3fc
priyansh0202-coder <priyanshsoni36@gmail.com>
2024-06-21 18:28:19 +0530

    ..

--- server.js
+++ server.js
@@ -10,20 +10,17 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.use((req, res, next) => {
-    res.setHeader("Content-Security-Policy", "default-src 'self'; font-src 'self' data:; style-src 'self' 'unsafe-inline';");
-    next();
-});
-
 app.get('/api/news', async (req, res) => {
     const { category } = req.query;
     const apiKey = process.env.VITE_NEWS_API_KEY;
     const url = `https://newsapi.org/v2/top-headlines?country=in&category=${category}&pageSize=100&apiKey=${apiKey}`;
 
     try {
+        console.log(`Fetching news for category: ${category}`);
         const response = await axios.get(url);
         res.json(response.data);
     } catch (error) {
+        console.error('Error fetching news:', error.message);
         res.status(500).json({ error: error.message });
     }
 });
