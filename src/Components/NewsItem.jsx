4166e1941447b3ab115278d480ddbb9033a81fb1
priyansh0202-coder <priyanshsoni36@gmail.com>
2024-06-21 14:29:06 +0530

    ..

--- src/Components/NewsItem.jsx
+++ src/Components/NewsItem.jsx
@@ -11,43 +11,22 @@ const NewsItem = ({ category, searchQuery }) => {
     const [currentPage, setCurrentPage] = useState(1);
     const recordPerPage = 12;
 
-    // useEffect(() => {
-    //     // let key = import.meta.env.VITE_NEWS_API_KEY;
-    //     let url = `https://newsapi.org/v2/top-headlines?country=in&category=${category}&pageSize=100&apiKey=e9a947a395174904ae5d9e8662d757ae`;
-
-    //     const fetchArticles = async () => {
-    //         try {
-    //             const response = await axios.get(url);
-    //             setArticles(response.data.articles)
-
-    //         } catch (error) {
-    //             console.log("error fetching the news articles:", error)
-    //         }
-    //     }
-    //     fetchArticles();
-    // }, [category]);
-
     useEffect(() => {
+        // let key = import.meta.env.VITE_NEWS_API_KEY;
+        let url = `https://newsapi.org/v2/top-headlines?country=in&category=${category}&pageSize=100&apiKey=e9a947a395174904ae5d9e8662d757ae`;
+
         const fetchArticles = async () => {
             try {
-                const response = await axios.get(`https://newsapi.org/v2/top-headlines`, {
-                    params: {
-                        country: 'in',
-                        category: category,
-                        pageSize: 100,
-                        apiKey: e9a947a395174904ae5d9e8662d757ae
-                    },
-                    headers: {
-                        'Upgrade-Insecure-Requests': 1
-                    }
-                });
-                setArticles(response.data.articles);
+                const response = await axios.get(url);
+                setArticles(response.data.articles)
+
             } catch (error) {
-                console.error("Error fetching the news articles:", error);
+                console.log("error fetching the news articles:", error)
             }
-        };
+        }
         fetchArticles();
     }, [category]);
+
 
 
     useEffect(() => {
